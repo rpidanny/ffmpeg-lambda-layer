f824e1ec032dfdca4dfe0df78eb129118e79758c
rpidanny <abhishekmaharjan1993@gmail.com>
2019-06-18 09:41:47 +0200

    Fix version

--- publish.sh
+++ publish.sh
@@ -27,5 +27,5 @@ for region in $REGIONS; do
   aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME \
     --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
     --version-number $(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --zip-file fileb://layer.zip \
-      --description "FFMPEG ${GIT_VERSION} binaries" --query Version --output text --profile $PROFILE) --profile $PROFILE
+      --description "FFMPEG ${FFPMEG_VERSION} binaries" --query Version --output text --profile $PROFILE) --profile $PROFILE
 done
